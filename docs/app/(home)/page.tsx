4776e044b994013d54c2707b1edba38339a8068f
burnedchris <christopher@everfund.com>
2025-01-14 15:18:09 +0000

    docs: :memo: Fixed docs meta + global

--- docs/app/(home)/page.tsx
+++ docs/app/(home)/page.tsx
@@ -2,6 +2,20 @@
 import { Features } from "@/components/sections/features";
 import { Footer } from "@/components/sections/footer";
 import { Hero } from "@/components/sections/hero";
+import { siteConfig } from "@/lib/config";
+import { Metadata } from "next";
+
+export const page = {
+  author: "Christopher Burns",
+  description:
+    siteConfig.description,
+  title: "Better Events",
+};
+
+export const metadata: Metadata = {
+  description: page.description,
+  title: page.title,
+};
 
 
 export default function Home() {
