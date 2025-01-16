bc38cdf7ed4068d7fec4fb3d05bb24f4295bf4ff
burnedchris <christopher@everfund.com>
2025-01-16 23:35:02 +0000

    feat: ✨ Add @vercel/analytics package and integrate analytics component
    - Introduced the @vercel/analytics package (version 1.4.1) to the project for enhanced analytics capabilities.
    - Updated layout to include the Analytics component for tracking user interactions.
    - Adjusted package versions for core packages to 0.0.1-beta.0 for consistency across the project.

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -3,7 +3,7 @@ import { RootProvider } from "fumadocs-ui/provider";
 import { Inter } from "next/font/google";
 import type { ReactNode } from "react";
 import { Body } from "./layout.client";
-
+import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({
 	subsets: ["latin"],
 });
@@ -13,6 +13,7 @@ export default function Layout({ children }: { children: ReactNode }) {
 		<html lang="en" className={inter.className} suppressHydrationWarning>
 			<Body>
 				<RootProvider>{children}</RootProvider>
+				<Analytics/>
 			</Body>
 		</html>
 	);
