fb5e3541921671034667213c5a69b2179bbaa090
burnedchris <christopher@everfund.com>
2025-01-16 23:48:11 +0000

    refactor: :bug: Tweaked shadcn build-script-locations

--- docs/registry/new-york/example/examples/consent-solution-minimal-demo.tsx
+++ docs/registry/new-york/example/examples/consent-solution-minimal-demo.tsx
@@ -2,16 +2,16 @@ import CookieBanner from "@/registry/default/components/consent/cookie-banner";
 import { ConsentManagerProvider } from "@koroflow/core-react";
 
 export default function PrivacyPopupMinimalDemo() {
-	return (
-		<ConsentManagerProvider
-			initialGdprTypes={[
-				"necessary",
-				"marketing",
-				"functionality",
-				"measurement",
-			]}
-		>
-			<CookieBanner />
-		</ConsentManagerProvider>
-	);
+  return (
+    <ConsentManagerProvider
+      initialGdprTypes={[
+        "necessary",
+        "marketing",
+        "functionality",
+        "measurement",
+      ]}
+    >
+      <CookieBanner />
+    </ConsentManagerProvider>
+  );
 }
--- docs/registry/registry-lib.ts
+++ docs/registry/registry-lib.ts
@@ -1,15 +1,15 @@
 import type { Registry } from "@/registry/schema";
 
 export const lib: Registry["items"] = [
-	{
-		name: "utils",
-		type: "registry:lib",
-		dependencies: ["clsx", "tailwind-merge"],
-		files: [
-			{
-				path: "lib/utils.ts",
-				type: "registry:lib",
-			},
-		],
-	},
+  {
+    name: "utils",
+    type: "registry:lib",
+    dependencies: ["clsx", "tailwind-merge"],
+    files: [
+      {
+        path: "lib/utils.ts",
+        type: "registry:lib",
+      },
+    ],
+  },
 ];
