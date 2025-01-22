29f9d58a1f84af57fcd780e94fb900d2faca1046
burnedchris <christopher@everfund.com>
2025-01-22 21:08:07 +0000

    style: :lipstick: fixed demo

--- docs/src/app/sdk-demo/page.tsx
+++ docs/src/app/sdk-demo/page.tsx
@@ -1,8 +1,13 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
 import KoroflowDevTool from "@koroflow/dev-tools";
-import { ConsentManagerProvider } from "@koroflow/elements/common";
-import { ConsentManagerDialog, ConsentManagerWidget } from "@koroflow/elements/consent-manager";
+import { ConsentManagerProvider, useConsentManager } from "@koroflow/elements/common";
+import { ConsentManagerDialog } from "@koroflow/elements/consent-manager";
 import CookieBanner from "@koroflow/elements/cookie-banner";
 import "@koroflow/elements/globals.css";
+import { Cookie, RefreshCw } from "lucide-react";
+import { useCallback } from "react";
 
 export default function PrivacyConsentPage() {
 	return (
@@ -20,15 +25,52 @@ export default function PrivacyConsentPage() {
 					},
 				}}
 			>
-				<div className="text-center space-y-4">
-					<h1 className="text-4xl font-bold">Koroflow SDK Demo</h1>
-					<p className="text-xl">Explore our privacy consent management tools</p>
-				</div>
+				<DemoWidget />
 				<CookieBanner />
 				<ConsentManagerDialog />
-				<ConsentManagerWidget />
 				<KoroflowDevTool />
 			</ConsentManagerProvider>
 		</main>
+	);
+}
+
+export function DemoWidget() {
+	const { clearAllData, setShowPopup, setIsPrivacyDialogOpen } = useConsentManager();
+	const handleResetConsent = useCallback(() => {
+		clearAllData();
+	}, [clearAllData]);
+
+	const handleOpenCookiePopup = useCallback(() => {
+		setShowPopup(true);
+	}, [setShowPopup]);
+
+	const handleOpenConsentCustomization = useCallback(() => {
+		setIsPrivacyDialogOpen(true);
+	}, [setIsPrivacyDialogOpen]);
+
+	return (
+		<div className="space-y-8">
+			<div className="text-center space-y-4 mb-8">
+				<h1 className="text-4xl font-bold">Koroflow SDK Demo</h1>
+				<p className="text-xl">Explore our privacy consent management tools</p>
+			</div>
+
+			<div className="flex flex-row gap-4 py-8 lg:py-0 items-center justify-center max-w-xl mx-auto">
+				<Button onClick={handleOpenCookiePopup}>
+					<Cookie className="h-4 w-4 mr-2" />
+					Open Cookie Banner
+				</Button>
+				<Button onClick={handleOpenConsentCustomization}>
+					<Cookie className="h-4 w-4 mr-2" />
+					Open Consent Dialog
+				</Button>
+			</div>
+			<div className="flex flex-row gap-4 py-8 lg:py-0 items-center justify-center max-w-xl mx-auto">
+				<Button onClick={handleResetConsent}>
+					<RefreshCw className="h-4 w-4 mr-2" />
+					Reset Local Storage Consent
+				</Button>
+			</div>
+		</div>
 	);
 }
