9fd1d8877e0d4ff5235d532ae0ef8fa902934323
burnedchris <christopher@everfund.com>
2025-01-28 16:08:52 +0000

    fix: :ambulance: Fixed OG Image and title and description on homepage

--- apps/docs/src/app/(home)/page.tsx
+++ apps/docs/src/app/(home)/page.tsx
@@ -1,11 +1,17 @@
-
 import { FeaturesSection } from "./_components/features";
 import { Hero } from "./_components/hero";
 
 import { CTA } from "./_components/cta";
 import { Examples } from "./_components/examples";
 import { Footer } from "./_components/footer";
-// import { ComponentsSection } from "./_components/components";
+import type { Metadata } from "next/types";
+
+export const metadata: Metadata = {
+	title: "Koroflow",
+	description:
+		"A modern privacy consent solution that gives you full visibility into user privacy choices. Get powerful analytics, consent tracking, and privacy controls in one fast package",
+};
+
 export default function HomePage() {
 	return (
 		<>
